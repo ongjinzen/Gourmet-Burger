458c6369a55ea17aac245f0c9b86c573b4c1102f
Jack Clarke <z5162865@student.unsw.edu.au>
2019-04-07 16:31:50 +1000

    commented system

--- doc/Assignment/system.py
+++ doc/Assignment/system.py
@@ -2,7 +2,7 @@ from order import Order
 from errors import *
 
 class System():
-
+    #initialise variables 
     def __init__(self, Inventory, Ingredient_Costs):
         self._Inventory = Inventory
         self._Ingredient_Costs = Ingredient_Costs
@@ -51,11 +51,13 @@ class System():
     @property
     def Incomplete_Orders(self):
         return self._Incomplete_Orders
-
+        
+    #creates a new order 
     def Create_Order(self):
         new_order = Order(self._Inventory, self._Ingredient_Costs)
         return new_order
 
+    #deletes an order when it is cancelled and restores the inventory
     def Delete_Order(self, order):
         
         if isinstance(order, Order):
@@ -67,6 +69,7 @@ class System():
         else:
             raise SystemError("Invalid order.")
 
+    #finalises an order and sends it to the list of incomplete orders
     def Submit_Order(self, order):
         
         if order.Status == None:
@@ -80,12 +83,14 @@ class System():
         else:
             raise SystemError("Order status error.")
 
+    #kitchen staff changing an order status when they are preparing it 
     def Preparing_Order(self, order):
         if order in self._Incomplete_Orders and order.Status == "Submitted":
             order.Status = "Preparing order"
         else:
             raise SystemError("Order status error.")
 
+    #Changes an orders status to complete once it is ready, also moves it to the Completed orders list
     def Complete_Order(self, order):
         if order in self._Incomplete_Orders and order.Status == "Preparing order":
             order.Status = "Completed"
@@ -94,6 +99,7 @@ class System():
         else:
             raise SystemError("Order status error.")
 
+    #returns the order when given a specific order ID, lokking through only orders submitted
     def View_Order(self, ID):
         for order in self._Incomplete_Orders:
             if ID == order.ID:
@@ -105,6 +111,7 @@ class System():
 
         raise SystemError("Order not found.")
 
+    #return the current status of an order once it has been submitted
     def Check_Status(self, ID):
         for order in self._Incomplete_Orders:
             if ID == order.ID:
@@ -116,6 +123,7 @@ class System():
 
         raise SystemError("Order not found.")
 
+    #returns a list of all orders that have been submitted
     def View_All_Orders(self):
         orders = []
         for order in self._Incomplete_Orders:
@@ -124,7 +132,8 @@ class System():
             orders.append(order)
 
         return orders
-    
+
+    #returns a list of all orders that have been submitted but have not been cooked yet
     def View_Incomplete_Orders(self):
         orders = []
         for order in self._Incomplete_Orders:
@@ -132,6 +141,7 @@ class System():
 
         return orders
 
+    #returns a list of all orders that are ready
     def View_Complete_Orders(self):
         orders = []
         for order in self._Completed_Orders:
@@ -139,18 +149,23 @@ class System():
 
         return orders
 
+    #increases the amount of stock of a speciifed ingrediant by a specified amount
     def Update_Stock(self, Ingredient, amount):
         self._Inventory[Ingredient] += amount
 
+    #Adds a new stock to the menu
     def New_Stock(self, Ingredient, price):
         self._Ingredient_Costs[Ingredient] = price
 
+    #returns a list of all main menu items
     def View_Main_Menu(self):
         return self._Main_Menu
 
+    #returns a list of all side menu items
     def View_Side_Menu(self):
         return self._Side_Menu
 
+    #returns a list of all drinks on the  menu 
     def View_Drink_Menu(self):
         return self._Drink_Menu
 
